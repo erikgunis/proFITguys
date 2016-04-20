dba8b4afa77527e480cbfb040a152453bf8e8b84
Seki_win8.1 <seki98@gmail.com>
2016-04-20 09:40:39 +0200

    +-*/ working

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,9 +12,44 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static class Globals
+        {
+            public static char operand;
+            public static float baseNumber = 0;
+            public static float secondNumber = 0;
+        }
+        private float add(float first, float second)
+        {
+            float result;
+            result = first + second;
+            return result;
+        }
+
+        private float subtract(float first, float second)
+        {
+            float result;
+            result = first - second;
+            return result;
+        }
+
+        private float multiply(float first, float second)
+        {
+            float result;
+            result = first * second;
+            return result;
+        }
+
+        private float divide(float first, float second)
+        {
+            float result;
+            result = first / second;
+            return result;
         }
 
         private void button19_Click(object sender, EventArgs e)
@@ -24,9 +59,19 @@ namespace Calculator
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            int baseNumber;
-            baseNumber = Int32.Parse(textBox1.Text);
-            MessageBox.Show("Current value is: " + baseNumber);
+            if ((Globals.operand == '\0'))
+            { 
+                if (!(String.IsNullOrEmpty(textBox1.Text))) {
+                    Globals.baseNumber = float.Parse(textBox1.Text);
+                }
+            }
+            if (!(Globals.operand == '\0'))
+            {
+                if (!(String.IsNullOrEmpty(textBox1.Text)))
+                {
+                    Globals.secondNumber = float.Parse(textBox1.Text);
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,19 +82,83 @@ namespace Calculator
 
         private void button_n0_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("You pressed 0");
+            
         }
 
         private void Form1_Press(object sender, KeyPressEventArgs e)
         {
-            
+            if (!(ActiveControl.GetType() == typeof(TextBox)))
+            {
+                textBox1.Text += e.KeyChar;
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.Focus();
+            }
+                
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.D0 || e.KeyCode == Keys.NumPad0)
+            int key = e.KeyValue;
+            if(e.KeyCode == Keys.D0)
             {
                // MessageBox.Show("0 pressed");
+            }
+
+            if (key == 107)
+            {
+                Globals.operand = '+';
+                textBox1.Clear();
+            }
+            if (key == 109)
+            {
+                Globals.operand = '-';
+                textBox1.Clear();
+            }
+            if (key == 106)
+            {
+                Globals.operand = '*';
+                textBox1.Clear();
+            }
+            if (key == 111)
+            {
+                Globals.operand = '/';
+                textBox1.Clear();
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                float answer = 0;
+                if (Globals.operand == '+')
+                {
+                    answer = add(Globals.baseNumber, Globals.secondNumber);
+                    Globals.operand = '\0';
+                    textBox1.Text = Convert.ToString(answer);
+                }
+                else if (Globals.operand == '-')
+                {
+                    answer = subtract(Globals.baseNumber, Globals.secondNumber);
+                    Globals.operand = '\0';
+                    textBox1.Text = Convert.ToString(answer);
+                }
+                else if (Globals.operand == '*')
+                {
+                    answer = multiply(Globals.baseNumber, Globals.secondNumber);
+                    Globals.operand = '\0';
+                    textBox1.Text = Convert.ToString(answer);
+                }
+                else if (Globals.operand == '/')
+                {
+                    if (Globals.secondNumber == 0)
+                    {
+                        textBox1.Text = "Cannot divide by zero!";
+                    } else {
+                        answer = divide(Globals.baseNumber, Globals.secondNumber);
+                        Globals.operand = '\0';
+                        textBox1.Text = Convert.ToString(answer);
+                    }
+                }
+
+
+                
             }
         }
 
