7a7cde7763c9bc9762f8515e321cd020ae1345e4
Mateas147 <radista4@gmail.com>
2016-04-25 16:04:00 +0200

    Added files via upload

--- Calculator/Mathlib.cs
+++ Calculator/Mathlib.cs
@@ -4,167 +4,172 @@ using System.Windows.Forms;
 
 
 public static class Mathlib
-{
-    public static double addition(double a, double b) //SCITANIE
     {
-        if (((a + b) > double.MaxValue) || ((a + b) < double.MinValue))
+    	/// <summary>
+    	/// Secteni prvniho a druheho cisla
+    	/// </summary>
+    	///
+    	/// <param name="a"> hodnota prvniho cisla </param>
+    	/// <param name="b"> hodnota druheho cisla </param>
+    	///
+    	/// <returns> vraceni hodnoty secteni prvniho a druheho cisla </returns>
+        public static float addition(float a, float b) //SCITANIE
         {
-            MessageBox.Show("Overflow", "Warning");         // overflow
-            return 0;
-        }
-        else
             return (a + b);
-    }
-
-    public static double subtraction(double a, double b) //ODCITANIE
-    {
-        if ((a - b > double.MaxValue) || (a - b < double.MinValue))
-        {
-            MessageBox.Show("Overflow", "Warning");         // overflow
-            return 0;
         }
-        else
+		/// <summary>
+		/// Odecteni prvniho a druheho cisla
+		/// </summary>
+		///
+		/// <param name="a"> hodnota prvniho cisla </param>
+		/// <param name="b"> hodnota druheho cisla </param>
+		///
+		/// <returns> vraceni hodnoty odecteni prvniho a druheho cisla </returns>
+        public static float subtraction(float a, float b) //ODCITANIE
+        {
             return (a - b);
-    }
-
-    public static double multiplication(double a, double b) //NASOBENIE
-    {
-
-        if ((a * b > double.MaxValue) || (a * b < double.MinValue))
-        {
-            MessageBox.Show("Overflow", "Warning");         // overflow
-            return 0;
         }
-        else
+        /// <summary>
+        /// Nasobeni prvniho a druheho cisla
+        /// </summary>
+        ///
+        /// <param name="a"> hodnota prvniho cisla </param>
+        /// <param name="b"> hodnota druheho cislaiption </param>
+        ///
+        /// <returns> vrceni hodnoty nasobeni prvniho a druheho cisla  </returns>
+        public static float multiplication(float a, float b) //NASOBENIE
+        {
             return (a * b);
-    }
-
-    public static double divide(double a, double b) //DELENIE
-    {
-        if (b == 0)
-        {
-            MessageBox.Show("Invalid format (Divide by zero)", "Warning");         // delenie nulou
-            return 0;
         }
-        else
+		/// <summary>
+		/// Deleni prvniho cisla druhym a osetreni pro deleni nulou
+		/// </summary>
+		///
+		/// <param name="a"> hodnota prvniho cisla </param>
+		/// <param name="b"> hodnota druheho cisla </param>
+		///
+		/// <returns> vraceni hodnoty prvniho cisla vydelene druhym cislem pokud druho cislo nebylo 0 </returns>
+        public static float divide(float a, float b) //DELENIE
         {
-            if ((a * b > double.MaxValue) || (a * b < double.MinValue))
+            if (b == 0)
             {
-                MessageBox.Show("Overflow", "Warning");         // overflow
+                MessageBox.Show("Invalid format(Divide by zero)", "Warning");         // delenie nulou
                 return 0;
             }
             else
+            {
                 return (a / b);
+            }
         }
-    }
-
-    public static double modulo(double a, double b)  // DELENIE MODULO
-    {
-        ulong aa = Convert.ToUInt64(a);
-        ulong bb = Convert.ToUInt64(b);
-        ulong x = aa;
-        ulong y = bb;
-        // MessageBox.Show("b,bb:" + b+ " " + bb);
+        /// <summary>
+        /// modulo dvou cisel a osetreni pro deleni nulou, osetreni vstupu pouze pro cela cisla
+        /// </summary>
+        ///
+        /// <param name="a"> hodnota prvniho cisla </param>
+        /// <param name="b"> hodnota druheho cisla </param>
+        ///
+        /// <returns> vraceni hodnoty prvniho cisla modulo druhym cislem </returns>
+        public static float modulo(float a, float b)  // DELENIE MODULO
+        {
+            ulong aa = Convert.ToUInt64(a);
+            ulong bb = Convert.ToUInt64(b);
+            ulong x = aa;
+            ulong y = bb;
+       // MessageBox.Show("b,bb:" + b+ " " + bb);
         if (!((a - aa == 0) && (b - bb == 0)))  //test ci je parameter cele cislo
-        {
-            MessageBox.Show("Invalid format (real number)", "Warning");         //zly vstup - realne cislo
-            return 0;
+            {
+                MessageBox.Show("Invalid format(real number)", "Warning");         //zly vstup - realne cislo
+                return 0;
+            }
+            if (y == 0)
+            {
+                MessageBox.Show("Invalid format(Divide by zero)", "Warning");          //zly vstup - delenie nulou
+                return 0;
+            }
+            else
+            {
+                ulong z = x / y;
+                return Convert.ToSingle(x - (y * z));
+            }
         }
-        if (y == 0)
-        {
-            MessageBox.Show("Invalid format (Divide by zero)", "Warning");          //zly vstup - delenie nulou
-            return 0;
-        }
-        else
-        {
-            ulong z = x / y;
-            return Convert.ToSingle(x - (y * z));
-        }
-    }
+        /// <summary>
+        /// vypocet faktorialu a osetreni zaporneho faktorialu, faktorialu realneho cisla
+        /// </summary>
+        ///
+        /// <param name="f"> hodnota cisla ze vstupu </param>
+        ///
+        /// <returns>< vraceni hodnoty faktorial vstupniho cisla ></returns>
 
-
-    public static double factorial(double f)  //FAKTORIAL
-    {
+        public static float factorial(float f)  //FAKTORIAL
+        {
         if (f < 0)
         {
-            MessageBox.Show("Invalid format (less than zero)", "Warning");               // zly vstup - menej ako 0
+            MessageBox.Show("Invalid format(less than zero)", "Warning");               // zly vstup - menej ako 0
             return 0;
-        }
-
-        ulong x = Convert.ToUInt64(f);
-
+        } 
+        
+            ulong x = Convert.ToUInt64(f);
+        
 
         if (!(f - (Convert.ToUInt64(f)) == 0))              //test ci je parameter cele cislo
-        {
-            MessageBox.Show("Invalid format (real number)", "Warning");        //zly vstup - realne cislo
-            return 0;
-        }
-
-
-        else
-        {
-            if (x == 0)       // faktorial 0 je 1
-            {
-                x = 1;
+            { 
+                MessageBox.Show("Invalid format(real number)", "Warning");        //zly vstup - realne cislo
+                return 0;
             }
+
+            
             else
             {
-                for (ulong i = (x - 1); i > 0; i--)
+                if (x == 0)       // faktorial 0 je 1
                 {
-                    x *= i;
+                    x = 1;
+                }
+                else
+                {
+                    for (ulong i = (x - 1); i > 0; i--)
+                    {
+                        x *= i;
+                    }
                 }
             }
+            return Convert.ToSingle(x);
         }
-        if (x > double.MaxValue)
+        /// <summary>
+        /// vypocet umocneni, osetreni proti preteceni
+        /// </summary>
+        ///
+        /// <param name="a"> mocnene cislo </param>
+        /// <param name="c"> exponent </param>
+        ///
+        /// <returns> vraceni hodnoty umocneneho cisla na zadany exponent </returns>
+        public static float exponent(double a, double c) //MOCNINA - EXPONENT
         {
-            MessageBox.Show("Overflow", "Warning");         // overflow
-            return 0;
-        }
-        else
-            return Convert.ToDouble(x);
-    }
-
-    public static double exponent(double a, double c) //MOCNINA - EXPONENT
-    {
-        if (c < 0)
-        {
-            MessageBox.Show("Invalid format (real number)", "Warning");        //zly vstup - realne cislo
-            return 0;
-        }
-
-        ulong b = Convert.ToUInt64(c);
-        if (!(c - b == 0))              //test ci je parameter cele cislo
-        {
-            MessageBox.Show("Invalid format (real number)", "Warning");        //zly vstup - realne cislo
-            return 0;
-        }
-
-        if (b == 0)
-        {
-            return 1; //exponent je 0    
-        }
-
-        if (b > 0)
-        {
-            double x = a;
-            for (ulong i = (b - 1); i > 0; i--)
+            ulong b = Convert.ToUInt32(c);
+            if (b > 0)
             {
-                a *= x;
-            }
-            if ((a <= double.MaxValue) && (a >= double.MinValue))
-            {
-                return a;
+                for (ulong i = (b - 1); i > 0; i--)
+                {
+                    a *= a;
+                }
+                if (a <= float.MaxValue)
+                {
+                    return Convert.ToSingle(a);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid format(Overflow)", "Warning");
+                    return 0;
+                }
             }
             else
             {
-                MessageBox.Show("Overflow", "Warning");
+                MessageBox.Show("Invalid format(exponent is less than zero)", "Warning");         // zly exponent
                 return 0;
             }
         }
-        else
-        {
-            MessageBox.Show("Invalid format (exponent is less than zero)", "Warning");         // zly exponent
-            return 0;
-        }
+
+
+
+
+
     }
-}
