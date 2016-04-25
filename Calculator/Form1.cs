b515f5a1451a4f4bf51e1e112041aede61f1d373
Seki_win8.1 <seki98@gmail.com>
2016-04-25 12:58:45 +0200

    long solution possible

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -13,118 +13,240 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
-       
-        public Form1()
+        
+    
+    public Form1()
         {
             InitializeComponent();
         }
         //Mathlib kniznica = new Mathlib();
 
         public static class Globals
-        {
+        {//global variables
             public static char operand;
-            public static bool negativeNumber = false;
             public static bool potentialNegative = false;
             public static bool operandset = false;
-            public static bool enterPressed = false;
-            public static float baseNumber = 0;
-            public static float secondNumber = 0;
-            public static float intNumber = '\0';
-            public static bool labelFirstNumber = false;
+            //public static bool enterPressed = false;
+            public static float TextBoxNumber = 0;
             public static bool resultSet = false;
-
+            public static string example;
         }
+        globs numops = new globs();//an instance of the globs class
+        
+
+        public class globs{//Class for the whole example, creates list for numbers and operands
+            public List<float> numbers = new List<float>();
+            public List<char> operands = new List<char>();
+            // float num;
+            int sizeNum;
+            int sizeOp;
+            public void addNum(float a)
+            {
+                numbers.Add(a);
+            }
+            public int getNumSize()
+            {
+                sizeNum = numbers.Count();
+                return sizeNum;
+            }
+            public float getNumItem(int item)
+            {
+                return numbers[item];
+            }
+            public void setValue(int i, float value)
+            {
+                numbers[i] = value;
+            }
+            public void addOp(char op)
+            {
+                operands.Add(op);
+            }
+            public int getOpSize()
+            {
+                sizeOp = operands.Count();
+                return sizeOp;
+            }
+            public char getOpItem(int item)
+            {
+                return operands[item];
+            }
+        }
+
         public void addCharToTextbox(char a)
-        {
-            
+        {//checks and adds a charracter to Textbox
             if (Globals.operandset)//clear textbox to insert second number
             {
-                Globals.enterPressed = false;
+                //Globals.enterPressed = false;
                 Globals.potentialNegative = true;
                 Globals.operandset = false;
                 textBox1.Clear();
-                if (Globals.resultSet && !Globals.enterPressed)
+                if (Globals.resultSet)// && !Globals.enterPressed)
                 {
-                    Globals.baseNumber = '\0';
                     setDefaults();
                 }
             }
+
+            if (Globals.resultSet && !Globals.operandset)
+            {
+                clearFunction();
+            }
+
             if (a != 'k')//If input is a button
             {
-                if (a == '.' && textBox1.Text.IndexOf('.') > -1)
-                {
+                if (a == '.' && textBox1.Text.IndexOf('.') > -1)//forbids more dots 
                     return;
+                else if (a == '-' && (textBox1.Text.IndexOf('-') > -1 || !Globals.potentialNegative))//forbids more minuses
+                    return;
+                else if (a == '-' && textBox1.Text.IndexOf('-') < -1 && Globals.potentialNegative)//allows minus for negative number
+                    {
+                    textBox1.SelectionStart = 0;
+                    textBox1.Text += a;
+                    textBox1.SelectionStart = textBox1.Text.Length;
+                    textBox1.Focus();
                 }
+                //else if()
                 else
-                {
+                {//input is a number from button
                     textBox1.Text += a;
                     textBox1.SelectionStart = textBox1.Text.Length;
                     textBox1.Focus();
                 }
             }
-            
         }
         public void clearFunction()
-        {
-            setDefaults();
-            textBox1.Clear();
-            Globals.baseNumber = 0;
-            Globals.secondNumber = 0;
-            Globals.intNumber = 0;
+        {//clear everything
+
+            Globals.TextBoxNumber = 0;
+            textBox1.Text = "0";
             label_operations.Text = "\0";
-        }
-        public void equalsFunction()
-        {
-            float answer = 0;
-            if (Globals.negativeNumber)
-            {
-                if (!Globals.enterPressed)
-                    Globals.secondNumber = Globals.secondNumber * -1;
-            }
-            if (Globals.operand == '+')
-            {
-                answer = Mathlib.addition(Globals.baseNumber, Globals.secondNumber);
-            }
-            else if (Globals.operand == '-')
-            {
-                answer = Mathlib.subtraction(Globals.baseNumber, Globals.secondNumber);
-            }
-            else if (Globals.operand == '*')
-            {
-                answer = Mathlib.multiplication(Globals.baseNumber, Globals.secondNumber);
-            }
-            else if (Globals.operand == '/')
-            {
-                answer = Mathlib.divide(Globals.baseNumber, Globals.secondNumber);
-            }
-            else if (Globals.operand == 'f')
-            {
-                answer = Mathlib.factorial(Globals.baseNumber);
-                Globals.operand = '!';
-            }
-            else if (Globals.operand == 'm')
-            {
-                answer = Mathlib.modulo(Globals.baseNumber, Globals.secondNumber);
-            }
-            else if (Globals.operand == 'e')
-            {
-                answer = Mathlib.exponent(Globals.baseNumber, Globals.secondNumber);
-            }
-
-
-            Globals.operandset = true;
-
-            Globals.potentialNegative = false;
-            label_operations.Text = Convert.ToString(Globals.baseNumber) + ' ' + Convert.ToString(Globals.operand) + ' ' + Convert.ToString(Globals.secondNumber) + " =" + Convert.ToString(answer);
-            Globals.baseNumber = answer;
-            Globals.resultSet = true;
-            Globals.enterPressed = true;
-            textBox1.Text = Convert.ToString(Globals.baseNumber);
+            Globals.example = "\0";
+            numops.setValue(0, 0);
+            Globals.operand = '\0';
+            Globals.operandset = false;
+            Globals.resultSet = false;
+            numops.numbers.RemoveAt(0);
             textBox1.SelectionStart = textBox1.Text.Length;
             textBox1.Focus();
         }
+        public void setValue(int y, float value)
+        {//Sets value of computed numbers, deletes the other one, deletes operand that's been used
+            numops.numbers.RemoveAt(y + 1);
+            numops.setValue(y, value);
+            numops.operands.RemoveAt(y);
+            
+        }
+        public void equalsFunction()
+        {//enter pressed
+            float answer = 0;
+            numops.addNum(Globals.TextBoxNumber);
+            Globals.example += ' ' + Convert.ToString(Globals.TextBoxNumber);
+
+            int sizenum = numops.getNumSize();
+            int sizeop = numops.getOpSize();
+            float[] num = numops.numbers.ToArray();
+            char[] op = numops.operands.ToArray();
+            //MessageBox.Show(Convert.ToString(sizenum +" " + sizeop));
+            
+            for (int y = 0; y < sizeop; y++)
+            {//factorial
+                switch (op[y])
+                {
+                    case 'f':
+                        Globals.operand = '!';
+                        break;
+                }
+            }
+            for (int y = 0; y < sizeop; y++)
+            {//exponent
+                switch (op[y])
+                {
+                    case 'e':
+                        num[y] = Mathlib.exponent(num[y], num[y + 1]);
+                        setValue(y, num[y]);
+                        sizeop = numops.getOpSize();
+                        num = numops.numbers.ToArray();
+                        y = -1;
+                        break;
+                }
+            }
+            for (int y = 0; y < sizeop; y++)
+            {//multiplication modulo division
+                switch (op[y])
+                {
+                    case '*':
+                        num[y] = Mathlib.multiplication(num[y], num[y + 1]);
+                        setValue(y, num[y]);
+                        sizeop = numops.getOpSize();
+                        num = numops.numbers.ToArray();
+                        y = -1;
+                        break;
+                    case '/':
+                        num[y] = Mathlib.divide(num[y], num[y + 1]);
+                        setValue(y, num[y]);
+                        sizeop = numops.getOpSize();
+                        num = numops.numbers.ToArray();
+                        y = -1;
+                        break;
+
+                    case 'm':
+                        num[y] = Mathlib.modulo(num[y], num[y + 1]);
+                        setValue(y, num[y]);
+                        sizeop = numops.getOpSize();
+                        num = numops.numbers.ToArray();
+                        y = -1;
+                        break;
+                }
+            }
+            for (int y = 0; y < sizeop; y++)
+            {//adition subtraction
+
+                switch (op[y])
+                {
+                    case '+':
+                        num[y] = Mathlib.addition(num[y], num[y + 1]);
+                        setValue(y, num[y]);
+                        sizeop = numops.getOpSize();
+                        num = numops.numbers.ToArray();
+                        y = -1;
+                        break;
+                    case '-':
+                        num[y] = Mathlib.subtraction(num[y], num[y + 1]);
+                        setValue(y, num[y]);
+                        sizeop = numops.getOpSize();
+                        num = numops.numbers.ToArray();
+                        y = -1;
+                        break;
+                }
+            }
+            
+                
+            
+            answer = num[0];
+            numops.setValue(0, answer);
+            Globals.operandset = false;
+
+            Globals.potentialNegative = false;
+            label_operations.Text = Globals.example + " = " + Convert.ToString(answer);
+            Globals.resultSet = true;
+            textBox1.Text = Convert.ToString(answer);
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.Focus();
+            MessageBox.Show(Convert.ToString(numops.getNumSize() + " " + numops.getOpSize()));
+        }
+
+
         public void setOperator(char op)
         {
+
+           
+            numops.addNum(Globals.TextBoxNumber);
+            numops.addOp(op);
+
+            int size = numops.getNumSize();
+            int sizeop = numops.getOpSize();
+            label_operations.Text += ' ' + Convert.ToString(numops.getNumItem(size - 1)) + ' ' + Convert.ToString(numops.getOpItem(sizeop - 1));
+            Globals.example += ' ' + Convert.ToString(numops.getNumItem(size - 1)) + ' ' + Convert.ToString(numops.getOpItem(sizeop - 1));
+
             setThings();
             Globals.operand = op;
         }
@@ -140,33 +262,27 @@ namespace Calculator
             Globals.resultSet = false;
             Globals.operandset = false;
             Globals.operand = '\0';
-            Globals.negativeNumber = false;
             Globals.potentialNegative = false;
-            Globals.secondNumber = '\0';
-            Globals.enterPressed = false;
+            //Globals.enterPressed = false;
         }
 
 
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if (Globals.resultSet && !Globals.enterPressed)
-            {
-                Globals.baseNumber = '\0';
-                setDefaults();
-            }
+            
             if ((Globals.operand == '\0'))
             {
                 if (!(String.IsNullOrEmpty(textBox1.Text)))
                 {
-                    Globals.baseNumber = float.Parse(textBox1.Text);
+                    Globals.TextBoxNumber = float.Parse(textBox1.Text);
                 }
             }
             if (!(Globals.operand == '\0'))
             {
-                if (!(string.IsNullOrEmpty(textBox1.Text)) && (!Globals.enterPressed))
+                if (!(string.IsNullOrEmpty(textBox1.Text)) && !(textBox1.Text == "-"))
                 {
-                    Globals.secondNumber = float.Parse(textBox1.Text);
+                    Globals.TextBoxNumber = float.Parse(textBox1.Text);
                    
                 }
             }
@@ -181,51 +297,58 @@ namespace Calculator
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {//https://msdn.microsoft.com/en-us/library/aa243025(v=vs.60).aspx
+        {//https://msdn.microsoft.com/en-us/library/aa243025(v=vs.60).asxp
 
-            if (Globals.operandset && (e.KeyValue >= 48 && e.KeyValue <= 57))
-            {
+            //if (Globals.operandset && (e.KeyValue >= 48 && e.KeyValue <= 57))
+            if ((e.KeyValue >= 48 && e.KeyValue <= 57))
+            {//keyboard number pressed
                 addCharToTextbox('k');
             }
            
-            
-            int key = e.KeyValue;
+
+            int key = e.KeyValue;//keyboard button value
+            if (key == 81)
+            {//q - Quit keyboard pressed
+                Application.Exit();   
+            }
             if ( key == 67 )
-            {//C = clear
+            {//C - Clear keyboard pressed
                 clearFunction();
             }
             if (key == 107)
-            {
+            {//addition keyboard pressed
                 setOperator('+');
             }
             if (key == 109)
-            {
+            {//substraction keyboard pressed         
                 if (Globals.potentialNegative)
                 {
                     Globals.potentialNegative = false;
-                    Globals.negativeNumber = true;
-                } else {
+                }
+                else
+                {
                     setOperator('-');
                 }
+                addCharToTextbox('-');
             }
             if (key == 106)
-            {
+            {//multiplication keyboard pressed
                 setOperator('*');
             }
             if (key == 111)
-            {
+            {//division keyboard pressed
                 setOperator('/');
             }
             if (key == 70)
-            {
+            {//fact keyboard pressed
                 setOperator('f');
             }
             if (key == 77)
-            {
+            {//mod keyboard pressed
                 setOperator('m');
             }
             if (key == 69)
-            {
+            {//e keyboard pressed
                 setOperator('e');
                 textBox1.Clear();
             }
@@ -238,7 +361,7 @@ namespace Calculator
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
-            {
+            {//numbers to textBox1 check
                 e.Handled = true;
             }
 
@@ -247,6 +370,7 @@ namespace Calculator
             {
                 e.Handled = true;
             }
+            
         }
 
     
@@ -322,10 +446,10 @@ namespace Calculator
             if (Globals.potentialNegative)
                 {
                     Globals.potentialNegative = false;
-                    Globals.negativeNumber = true;
                 } else {
                     setOperator('-');
                 }
+            addCharToTextbox('-');
         }
 
         private void button_mul_Click(object sender, EventArgs e)
@@ -358,5 +482,9 @@ namespace Calculator
         {
             clearFunction();
         }
+
+           
+        
+
     }
 }
